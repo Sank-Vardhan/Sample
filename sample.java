ef55ed9bf3b5434b2cb9f53bf5c60285e80064ba
root <root@ip-172-31-52-116.ec2.internal>
2021-01-22 18:51:12 +0000

    last commit of the day

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 class simple{
 	public static void main(String[] args){
+		System.out.println("------------------------------BUILDING PROJECT ON JENKINS---------------------------------");
 		System.out.println("HELLO WORLD WELCOME TO DEVOPS");
 	}
 }
